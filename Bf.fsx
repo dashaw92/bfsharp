48c90c7ebc648a5285dc2bdb1fc99b11a29f24b1
dashaw92 <dashaw92@gmail.com>
2023-10-17 19:54:38 -0400

    Use defaultArg, remove ioBuf

--- Bf.fsx
+++ Bf.fsx
@@ -7,7 +7,6 @@ BF interpreter in F# (BF#)
 
 open System
 
-
 /// Represents all possible Bf instructions:
 ///     IncPtr:     >
 ///         Increments the memory pointer by 1
@@ -62,21 +61,19 @@ type State =
         /// The key is just a simple index for lookups by Ptr
         Memory: Map<int, int>
     }
-    
+
+/// Define an infix operator for defaultArg
+let (??=) optA def = defaultArg optA def
+
 /// Apply `fn` to the cell at `ptr` in `memory`
 /// If the memory map does not contain the key already,
 /// apply `fn` to 0. Returns the new map
 let changeMemory memory ptr (fn: int -> int) =
-    let wrapOptFn = function
-        | Some value -> Some <| fn value
-        | None -> Some <| fn 0
+    let wrapOptFn cell = Some <| fn (cell ??= 0)
     Map.change ptr wrapOptFn memory
 
 /// Get the value of the current cell or 0
-let cellValue state =
-    match Map.tryFind state.Ptr state.Memory with
-    | Some cellVal -> cellVal
-    | None -> 0
+let cellValue state = (Map.tryFind state.Ptr state.Memory) ??= 0 
     
 /// Find the index in the instrs tape where the matching loop instruction is,
 /// or None if there is none (error case)
@@ -93,7 +90,7 @@ let rec findEndOfLoop ip instrs =
 /// Executes the input (instruction list) until either
 /// an error occurs (unmatched loop start/end) or the input has been
 /// exhausted.
-let run state (input: Instr list) (ioBuf: char list) =
+let run state (input: Instr list) =
     //Build up an index-able map of the instructions. aux will refer to this when
     //jumping back at the end of loops.
     let instrTape =
@@ -103,7 +100,9 @@ let run state (input: Instr list) (ioBuf: char list) =
     
     //Helper that closes over instrTape so it doesn't need to be
     //provided with every call
-    let rec aux state ip ioBuf =
+    let rec aux state ip =
+        //Clean up code
+        let nextIp = ip + 1
         //Use tryFind to manually handle out of bounds indexing
         match Map.tryFind ip instrTape with
         //ip is either invalid now, or we've reached the end of the input.
@@ -111,22 +110,22 @@ let run state (input: Instr list) (ioBuf: char list) =
         | Some instr ->
             match instr with
             //Basic ops, inc/dec the Ptr
-            | IncPtr -> aux { state with Ptr = state.Ptr + 1 } (ip + 1) ioBuf
-            | DecPtr -> aux { state with Ptr = state.Ptr - 1 } (ip + 1) ioBuf
+            | IncPtr -> aux { state with Ptr = state.Ptr + 1 } nextIp
+            | DecPtr -> aux { state with Ptr = state.Ptr - 1 } nextIp
             //Inc/dec the current cell's value. Note: Dec's fn is not ((-) 1) because it causes
             //strange behavior with negatives: ((-) 1) n = abs n???
-            | Inc -> aux { state with Memory = changeMemory state.Memory state.Ptr ((+) 1) } (ip + 1) ioBuf
-            | Dec -> aux { state with Memory = changeMemory state.Memory state.Ptr ((+) -1) } (ip + 1) ioBuf
+            | Inc -> aux { state with Memory = changeMemory state.Memory state.Ptr ((+) 1) } nextIp
+            | Dec -> aux { state with Memory = changeMemory state.Memory state.Ptr ((+) -1) } nextIp
             | MarkLoop ->
                 //If the current cell is 0, find the matching loop end (']') and jmp to 1 past it.
                 //If there is no matching ']', execution cannot continue because the program is invalid. 
                 if cellValue state = 0 then
                     match findEndOfLoop ip instrTape with
-                    | Some endLoop -> aux state endLoop ioBuf
+                    | Some endLoop -> aux state endLoop
                     | None -> state
                 else
                     //Save ip to the stack and begin the loop (go to the next instr like normal)
-                    aux { state with Stack = ip :: state.Stack } (ip + 1) ioBuf
+                    aux { state with Stack = nextIp :: state.Stack } nextIp
             | JmpLoop ->
                 match state.Stack with
                 //An empty stack here means there's an unmatched ']'.
@@ -137,37 +136,30 @@ let run state (input: Instr list) (ioBuf: char list) =
                     //If the current cell is 0, the loop is done and we can continue to the
                     //next instruction like normal.
                     if cellValue state = 0 then
-                        aux { state with Stack = jmps } (ip + 1) ioBuf
+                        aux { state with Stack = jmps } nextIp
                     //Otherwise, return back to the start of the loop (1 beyond the '[' instruction so as to not
                     //keep starting a new loop with every jmp)
                     else
-                        aux state (jmp + 1) ioBuf
+                        aux state jmp
             | Print ->
                 //Read the current cell and print it as a char
                 let ch = char <| cellValue state
                 printf $"%c{ch}"
-                aux state (ip + 1) ioBuf
+                aux state nextIp
             | Input ->
-                //Read a single char from ioBuf or stdin and place it at the current
+                //Read a single char from stdin and place it at the current
                 //memory cell as an int
-                let ch, nextIoBuf =
-                    //If ioBuf is empty, use Console.Read to get the char.
-                    //Otherwise, use the head of ioBuf.
-                    match ioBuf with
-                    | [] -> Console.Read (), []
-                    | ch :: chars -> int ch, chars
-                aux { state with Memory = changeMemory state.Memory state.Ptr (fun _ -> ch) } (ip + 1) nextIoBuf
+                aux { state with Memory = changeMemory state.Memory state.Ptr (fun _ -> Console.Read ()) } nextIp
     //Run the program! IP = 0, state is user provided
-    aux state 0 ioBuf
+    aux state 0
 
 /// Convert a program in string form into a Instr list, filtering out invalid BF commands.
-let toInstr: char seq -> Instr list =
-    Seq.choose instrFromChar >> Seq.toList
+let toInstr: char seq -> Instr list = Seq.choose instrFromChar >> Seq.toList
 
 //Helper for running a program
 //runProgram "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++." []
 //-> Hello World!
-let runProgram program buf =
+let runProgram program =
     let st = { Ptr = 0; Stack = []; Memory = Map.empty }
     let instrs = toInstr program
-    run st instrs buf
+    run st instrs 
